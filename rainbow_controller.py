5aa86758fb68eb3538e46b621998c9b38bdf590d
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 16:09:51 -0600

    fixed some linting issues

--- rainbow_controller.py
+++ rainbow_controller.py
@@ -23,18 +23,18 @@ class RainbowController:
 
     def process_event(self, event):
         self.color_off = self.color_on
-        if (self.next_light % (self.num_lights * 2) == 0):
+        if self.next_light % (self.num_lights * 2) == 0:
             self.next_light = 0
 
         # pick the next color to turn on
-        self.color_on = self.wheel(self.next_light * 256 // self.num_lights )
+        self.color_on = wheel(self.next_light * 256 // self.num_lights )
 
         # Only turn on one light for a chord
         message, deltatime = event
         if deltatime < 0.02:
             print('chord')
             return
-       
+
         print(message, deltatime)
         state = message[0]
 
@@ -45,24 +45,24 @@ class RainbowController:
             self.pixels[self.prev_light % self.num_lights] = self.color_off
             self.prev_light+=1
 
-    ##Funtion that makes each light a different rainbow color
-    def wheel(self, pos): 
-        if pos<0 or pos > 255:
-            r= g= b=0
-        elif pos<85:
-            r= int(pos*3)
-            g= int(255-pos*3)
-            b=0
-        elif pos<170:
-            pos -=85
-            r= int(255 -pos*3)
-            g=0
-            b=int(pos*3)
-        else:
-            pos-= 170
-            r=0
-            g=int(pos*3)
-            b=int(255-pos*3)
-        return (r,g,b)
+##Funtion that makes each light a different rainbow color
+def wheel(pos):
+  if pos<0 or pos > 255:
+      red = green = blue = 0
+  elif pos<85:
+      red = int(pos*3)
+      green = int(255-pos*3)
+      blue =0
+  elif pos<170:
+      pos -=85
+      red = int(255 -pos*3)
+      green = 0
+      blue = int(pos*3)
+  else:
+      pos-= 170
+      red = 0
+      green = int(pos*3)
+      blue = int(255-pos*3)
+  return (red,green,blue)
 
 
