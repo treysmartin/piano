0f1d32cc3b43a3b677d102758c84cbb0832a14c2
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 15:08:10 -0600

    Fixing main method

--- better_design/main.py
+++ better_design/main.py
@@ -2,6 +2,6 @@ from simple_controller import SimpleController
 from piano_lights import LightsFromPiano
 
 lights_controller = SimpleController(5, (244,0,0), (0,0,255))
-piano_input = LightsFromPiano(50, (244,0,0),(0,0,255))
+piano_input = LightsFromPiano()
 piano_input.run()
 
