9ba6dae838d2a3ca3f17a8c149bc64ce9801806f
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 15:09:44 -0600

    fixing arguments passed to run

--- better_design/main.py
+++ better_design/main.py
@@ -3,5 +3,5 @@ from piano_lights import LightsFromPiano
 
 lights_controller = SimpleController(5, (244,0,0), (0,0,255))
 piano_input = LightsFromPiano()
-piano_input.run()
+piano_input.run(lights_controller)
 
