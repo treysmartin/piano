8ed2f8b0339753f7e43a22ede1c9a8607a34ba8d
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 16:12:05 -0600

    fixed indnetation

--- rainbow_controller.py
+++ rainbow_controller.py
@@ -47,22 +47,22 @@ class RainbowController:
 
 ##Funtion that makes each light a different rainbow color
 def wheel(pos):
-  if pos<0 or pos > 255:
-      red = green = blue = 0
-  elif pos<85:
-      red = int(pos*3)
-      green = int(255-pos*3)
-      blue =0
-  elif pos<170:
-      pos -=85
-      red = int(255 -pos*3)
-      green = 0
-      blue = int(pos*3)
-  else:
-      pos-= 170
-      red = 0
-      green = int(pos*3)
-      blue = int(255-pos*3)
-  return (red,green,blue)
+    if pos<0 or pos > 255:
+        red = green = blue = 0
+    elif pos<85:
+        red = int(pos*3)
+        green = int(255-pos*3)
+        blue =0
+    elif pos<170:
+        pos -=85
+        red = int(255 -pos*3)
+        green = 0
+        blue = int(pos*3)
+    else:
+        pos-= 170
+        red = 0
+        green = int(pos*3)
+        blue = int(255-pos*3)
+    return (red,green,blue)
 
 
