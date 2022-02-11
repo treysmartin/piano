7f6d06077dd387e335de5f9464ca02921ef9555e
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 15:05:33 -0600

    Fixed undefined names

--- better_design/main.py
+++ better_design/main.py
@@ -1,5 +1,5 @@
-import simple_controller
-import piano_lights
+import simple_controller.SimpleController
+import piano_lights.LightsFromPiano
 
 lights_controller = SimpleController(5, (244,0,0), (0,0,255))
 piano_input = LightsFromPiano(50, (244,0,0),(0,0,255))
