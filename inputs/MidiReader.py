4365b2cdaf21c38062a67b9dca4004e4f51fbf8d
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 09:45:07 -0600

    tangled up the design for a bad example

--- inputs/MidiReader.py
+++ inputs/MidiReader.py
@@ -1,15 +1,22 @@
 import rtmidi.midiutil as midiutil
 import queue
 
-class PianoReader:
+class MidiReader:
     def __init__(self):
         self.eventQ = queue.Queue(1024)
-        print("Starting reading from piano")
+        self.terminate = True;
         midiin, port_name = midiutil.open_midiinput(1)
         midiin.set_callback(self)
 
     def __call__(self, event, data=None):
         self.eventQ.put(event);
 
-    def getEventQueue(self):
-        return self.eventQ
+    def run(self, lightsController):
+        self.terminate = False
+        while not self.terminate:
+            event = self.eventQ.get()
+            lightsController.processEvent(event)
+
+    def stop(self):
+        self.terminate = True
+
