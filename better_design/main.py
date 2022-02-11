76124f935d21b069134027ea40446e4187128eee
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 15:07:25 -0600

    Fixing name lookup

--- better_design/main.py
+++ better_design/main.py
@@ -1,5 +1,5 @@
-import simple_controller.SimpleController
-import piano_lights.LightsFromPiano
+from simple_controller import SimpleController
+from piano_lights import LightsFromPiano
 
 lights_controller = SimpleController(5, (244,0,0), (0,0,255))
 piano_input = LightsFromPiano(50, (244,0,0),(0,0,255))
