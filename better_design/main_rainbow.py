8d32c187d8f8e2ba896bee8752d743bc3a35290a
Kate Holdener <kate.holdener@slu.edu>
2022-02-11 15:39:04 -0600

    removed better deisng

--- better_design/main_rainbow.py
+++
@@ -1,7 +0,0 @@
-from rainbow_controller import RainbowController
-from piano_lights import LightsFromPiano
-
-lights_controller = RainbowController(50)
-piano_input = LightsFromPiano()
-piano_input.run(lights_controller)
-
